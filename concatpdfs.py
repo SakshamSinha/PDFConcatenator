1a0ee5493e5d87e1b7464050081533c5d267ad2d
Saksham Sinha <saksham.sinha@intel.com>
2022-05-09 19:02:24 +0100

    added concat feature back

--- concatpdfs.py
+++ concatpdfs.py
@@ -10,22 +10,26 @@ def pdf_splitter(path):
     fname = os.path.splitext(os.path.basename(path))[0]
 
     pdf = PdfFileReader(path)
-    for page in range(pdf.getNumPages()):
-        pdf_writer = PdfFileWriter()
+    pdf_writer = PdfFileWriter()
+    for page in range(144, pdf.getNumPages(), 1):
+        if page==152:
+            break
+        
         pdf_writer.addPage(pdf.getPage(page))
 
-        output_filename = '{}_page_{}.pdf'.format(
-            fname, page + 1)
+    output_filename = '{}_page_{}.pdf'.format(
+        fname, page + 1)
+    with open(output_filename, 'wb') as out:
+        pdf_writer.write(out)
 
-        with open(output_filename, 'wb') as out:
-            pdf_writer.write(out)
-
-        print('Created: {}'.format(output_filename))
+    print('Created: {}'.format(output_filename))
 
 
 output = PdfFileWriter()
 dir = 'pdfs/'
+#to split pdfs uncomment below
 # pdf_splitter(dir+'Orignal.pdf')
+#to concatenate pdfs uncomment below
 for pdf in filter(lambda x: '.pdf' in x, sorted(os.listdir(dir))):
     print(pdf)
     # reader = PdfFileReader(dir+pdf)
